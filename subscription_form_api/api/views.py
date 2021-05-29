5856c92d949caf7a672f65ffc275b2b06346e13b
Moreno Pinheiro <moreno.pinheiro@gmail.com>
2021-05-28 21:27:38 -0300

    Create and list subscriptions using ajax

--- subscription_form_api/api/views.py
+++ subscription_form_api/api/views.py
@@ -8,6 +8,6 @@ from subscriptions.serializers import SubscriptionSerializer
 class SubscriptionListCreateAPIView(generics.ListCreateAPIView):
     """Create and list customer subscriptions"""
 
-    queryset = Subscription.objects.all()
+    queryset = Subscription.objects.order_by('-id')[:10]
     serializer_class = SubscriptionSerializer
     permission_classes = (AllowAny, )
